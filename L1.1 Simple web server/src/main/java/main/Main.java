11f09577ae25b9460432f187a5fbafa808d95797
sergii.volosheniuk <sergii.volosheniuk@globallogic.com>
2016-11-17 19:49:01 +0200

    The 1st homework

--- L1.1 Simple web server/src/main/java/main/Main.java
+++ L1.1 Simple web server/src/main/java/main/Main.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
+import servlets.CustomServlet;
 
 /**
  * @author v.chibrikov
@@ -15,14 +16,17 @@ import servlets.AllRequestsServlet;
 public class Main {
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        CustomServlet customServlet = new CustomServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(customServlet), "/mirror");
 
         Server server = new Server(8080);
         server.setHandler(context);
 
         server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 }
---
+++ L1.1 Simple web server/src/main/java/servlets/CustomServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String key = request.getParameter("key");
+
+        response.getWriter().println(key);
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+}
